bb7e74d64343e1b5dde78c76b91df2348895d2f8
undcvr <diyor.iknu@gmail.com>
2024-04-10 14:48:45 +0500

    mat ebal

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 document.querySelector('.calculate').addEventListener('click', function() {
     document.querySelector('.ring').style.display = 'block'
+    var audio = new Audio('./untitled.mp3');
+    audio.play();
     setTimeout(() => {
         document.querySelector('.ring').style.display = 'none'
         document.querySelector('img').style.height = '580px'
